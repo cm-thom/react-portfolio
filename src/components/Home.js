c3c7b53569468a487ec7ac4e267053356bfffeeb
Cole <122782745+cm-thom@users.noreply.github.com>
2023-07-24 23:24:58 -0400

    portfolio updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,21 +4,19 @@ export default function Home() {
   //logic needed for home
 
   return (
-    <div>
       <div className="actual-content">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
         commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur.
+        velit esse cillum dolore eu fugiat nulla pariatur.</p>
         <div>
-          <img
-            clasName="headshot"
+        <img
+            className="headshot"
             src="https://placekitten.com/200/300"
             alt="cat"
           ></img>
         </div>
       </div>
-    </div>
   );
 }
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -7,16 +7,24 @@ export default function Portfolio() {
 
   return (
     <div className="actual-content">
-      portfolio content
-      <Card style={{ width: "18rem" }}>
+      <Card className="project-card" style={{ width: "22rem" }}>
         <Card.Img variant="top" src="https://placekitten.com/100/100" />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+          <Card.Title>Bon Voyage</Card.Title>
           <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+            Vacation planning site for collaborating with friends
           </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
+          <Button variant="primary">View Site</Button>
+        </Card.Body>
+      </Card>
+      <Card className="project-card" style={{ width: "22rem" }}>
+        <Card.Img variant="top" src="https://placekitten.com/100/100" />
+        <Card.Body>
+          <Card.Title>Bon Voyage</Card.Title>
+          <Card.Text>
+            Vacation planning site for collaborating with friends
+          </Card.Text>
+          <Button variant="primary">View Site</Button>
         </Card.Body>
       </Card>
     </div>
